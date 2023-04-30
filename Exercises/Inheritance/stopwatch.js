bf9277051d0da51811c8f8c38022ec5e93a45db5
Johnny <JohnnyLieu@yahoo.com>
2023-04-29 17:32:17 -0700

    edited readme

--- Exercises/Inheritance/stopwatch.js
+++ Exercises/Inheritance/stopwatch.js
@@ -1,18 +1,94 @@
 
+// starting code
+// to do: move the methods start(), stop(), and reset() to protype
+
+// function Stopwatch() { 
+//   let startTime, endTime, running, duration = 0;
+
+//   this.start = function() {
+//     if (running) 
+//       throw new Error('Stopwatch has already started.');
+    
+//     running = true; 
+
+//     startTime = new Date();
+//   };
+
+//   this.stop = function() {
+//     if (!running) 
+//       throw new Error('Stopwatch is not started.');
+
+//     running = false; 
+      
+//     endTime = new Date();
+
+//     const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+//     duration += seconds; 
+//   };
+
+//   this.reset = function() { 
+//     startTime = null;
+//     endTime = null;
+//     running = false; 
+//     duration = 0; 
+//   };
+
+//   Object.defineProperty(this, 'duration', {
+//     get: function() { return duration; }
+//   });
+// }
+
+
+// my completed code
+
 function Stopwatch() { 
   let startTime, endTime, running, duration = 0;
 
-  this.start = function() {
-    if (running) 
+  // this.start = function() {
+  //   if (running) 
+  //     throw new Error('Stopwatch has already started.');
+    
+  //   running = true; 
+
+  //   startTime = new Date();
+  // };
+
+  // this.stop = function() {
+  //   if (!running) 
+  //     throw new Error('Stopwatch is not started.');
+
+  //   running = false; 
+      
+  //   endTime = new Date();
+
+  //   const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+  //   duration += seconds; 
+  // };
+
+  // this.reset = function() { 
+  //   startTime = null;
+  //   endTime = null;
+  //   running = false; 
+  //   duration = 0; 
+  // };
+
+  Object.defineProperty(this, 'duration', {
+    get: function() { return duration; }
+  });
+}
+
+Stopwatch.prototype.start = function() { // getter for return function
+  if (running) 
       throw new Error('Stopwatch has already started.');
     
     running = true; 
 
     startTime = new Date();
-  };
+}
 
-  this.stop = function() {
-    if (!running) 
+// adding the methods to prototype
+Stopwatch.prototype.stop = function() {
+  if (!running) 
       throw new Error('Stopwatch is not started.');
 
     running = false; 
@@ -20,17 +96,12 @@ function Stopwatch() {
     endTime = new Date();
 
     const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds; 
-  };
+    duration += seconds;
+}
 
-  this.reset = function() { 
-    startTime = null;
-    endTime = null;
-    running = false; 
-    duration = 0; 
-  };
-
-  Object.defineProperty(this, 'duration', {
-    get: function() { return duration; }
-  });
+Stopwatch.prototype.reset = function() {
+  startTime = null;
+  endTime = null;
+  running = false; 
+  duration = 0; 
 }
